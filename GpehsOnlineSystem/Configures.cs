94a77fe12b1505e63ce3f31ba96e2a72d0024608
영환 김 <napoleon27y@gmail.com>
2024-10-21 18:39:24 +0900

    초기 커밋.

---
+++ GpehsOnlineSystem/Configures.cs
@@ -0,0 +1,6 @@
+﻿namespace GpehsOnlineSystem;
+
+public class ApiKeys
+{
+    public string? SfLicense { get; set; }
+}
---
+++ GpehsOnlineSystem/Program.cs
@@ -0,0 +1,42 @@
+using GpehsOnlineSystem;
+using GpehsOnlineSystem.Client.Pages;
+using GpehsOnlineSystem.Components;
+using Syncfusion.Blazor;
+using Syncfusion.Licensing;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var sfKey = builder.Configuration.GetSection("ApiKeys").Get<ApiKeys>();
+SyncfusionLicenseProvider.RegisterLicense(sfKey?.SfLicense);
+
+builder.Services.AddSyncfusionBlazor();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(GpehsOnlineSystem.Client._Imports).Assembly);
+
+app.Run();
